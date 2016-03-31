7bb8b5c04db42adcfe2cc587d0bebc7cb1da2f56
Ksenia <gueletina.ksenia@gmail.com>
2016-03-31 17:34:11 -0400

    set up random numbers and operator for the play_round method

--- math_game.rb
+++ math_game.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
-def play_game()
-  
+@player_stats = {
+  player_1: {
+    name: "player1",
+    score: 0
+  },
+  player_2: {
+    name: "player2",
+    score: 0
+  }
+}
+
+def play_round(player)
+  num1 = rand(1..20)
+  num2 = rand(1..20)
+  operator = ["+","-","/","*"].sample
+  answer = eval("#{num1}#{operator}#{num2}")
+  puts "#{player[:name]}: What is #{num1} #{operator} #{num2}?"
 end
